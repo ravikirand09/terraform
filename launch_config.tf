61f406e1d8a127b8d141c0f6422be10481a84ca6
ravikirand09 <99682786+ravikirand09@users.noreply.github.com>
2023-04-03 17:45:27 -0400

    Updated launch_config.tf

--- launch_config.tf
+++ launch_config.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "web" {
 image_id = "ami-00c39f71452c08778" 
   instance_type = "t2.micro"
   key_name = "tests"
-security_groups = [ "${aws_security_group.demosg.id}" ]
+security_groups = [ "${aws_security_group.appsg1.id}" ]
   associate_public_ip_address = true
   user_data = "${file("data.sh")}"
 lifecycle {
