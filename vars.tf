59f0fbc0e688e7a2e1c395133d8e87b982c58743
ravikirand09 <99682786+ravikirand09@users.noreply.github.com>
2023-04-03 17:41:48 -0400

    Updated vars.tf

--- vars.tf
+++ vars.tf
@@ -16,10 +16,10 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 # Defining CIDR Block for Subnet
-variable "subnet_cidr" {
+variable "subnet1_cidr" {
   default = "10.0.1.0/24"
 }
 # Defining CIDR Block for 2d Subnet
-variable "subnet1_cidr" {
+variable "subnet2_cidr" {
   default = "10.0.2.0/24"
 }
